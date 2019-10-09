577c31ea0835ebab6ff7c7e4f78b5b75d15c1fb1
CTeodora <33898420+CTeodora@users.noreply.github.com>
2019-10-09 16:40:26 +0300

    App and assets

---
+++ application/views/layout/footer.php
@@ -0,0 +1,35 @@
+
+</body>
+
+<footer>
+  <section class="footer-image section_gap_top">
+<div class="container">
+<div class="row align-items-center justify-content-center">
+<div class="col-lg-6 offset-lg-6">
+<div class="contact-section">
+<form class="form">
+  <h2>CONTACT</h2>
+  <p type="Name:"><input></input></p>
+  <p type="Email:"><input></input></p>
+  <p type="Message:"><input></input></p>
+  <button class="button-form">Send Message</button>
+  <div class="contact-info">
+    <span class="fa fa-phone"></span>001 1023 567
+    <span class="fa fa-envelope-o"></span> contact@company.com
+  </div>
+</form>
+</div>
+</div>
+</div>
+</div>
+</section>
+</footer>
+
+<script>
+function pictureChange()
+{
+  document.getElementById("theImage").src="assets/img/6.jpg";
+}
+</script>
+
+</html>
---
+++ application/views/layout/header.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta content="width=device-width, initial-scale=1" name="viewport" />
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/style.css') ?>">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,800,300" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Lobster|Dancing+Script|Gayathri|Great+Vibes|Muli|Lato|Italianno|Lexend+Exa|Space+Poppins&display=swap" rel="stylesheet">
+
+	<title>Cup</title>	
+</head>
+
+
+<body class="body-background">
+	<div class="cover" style="background-image: url('assets/img/cover4.png');">
+		<div class="logo">
+			<img style="margin-top:1%;" src="assets/img/disposaLux-logo.svg" width="18%">
+		</div>
+		<div class="col-md-12">
+			<div class="row">
+				<div class="col-md-5 img-cover">
+					<img src="assets/img/4.png" width="100%">
+				</div>
+				<div class="col-md-5 right-section">
+					<!-- <h1 class="title">Try it now</h1> -->
+
+					<p>Lorem ipsum dolor sit amet.
+					Vivamus in elit hendrerit ipsum dolor.</p>
+					<p> Proin vel metus nec metus </p>
+					<div class="button-cover">
+						<button class="button">FIND MORE</button>
+					</div>
+					
+				</div>
+				
+			</div>
+		</div>
+	</div>
+
+	<div class="section-2">		
+	</div>
+
+	<div class="container">		
+		<div class="col-md-12">
+			<div class="row margin-top">
+				<div class="col-md-6 center-img">
+					<img id="theImage" src="assets/img/3.jpg" style="width: 100%">
+				</div>
+				<div class="col-md-6">
+					<ul class="c">
+				  		<li>Lorem ipsum dolor sit amet</li>
+				  		<li>Consectetur adipiscing</li>
+				  		<li>Sed a viverra leo</li>
+	  				</ul>
+	  				<input type="button" id="theButton" value="Change color" class="button-group" onclick="pictureChange()">
+				</div>
+			</div>
+		</div>
+	</div>
+		<div class="section-4">
+
+		</div>
+	<div class="container">
+		<div class="col-md-12">
+			<div class="row margin-top">
+				<div class="col-md-6 center-img">
+					<img src="assets/img/2.jpg" style="width: 100%">
+				</div>
+				<div class="col-md-6">
+					<ul class="c">
+				  		<li>Lorem ipsum dolor sit amet</li>
+				  		<li>Consectetur adipiscing</li>
+				  		<li>Sed a viverra leo</li>
+	  				</ul>
+	  				<button id="button-group" >Shop now</button>
+				</div>
+				
+			</div>
+		</div>
+
+		<!-- <div class="col-md-12">
+			<div class="row margin-top">
+				<div class="col-md-6 center-img">
+					<img src="assets/img/Lifestyle-3.jpg" style="width: 100%">
+				</div>
+				<div class="col-md-6">
+					<ul class="c">
+				  		<li>Lorem ipsum dolor sit amet</li>
+				  		<li>Consectetur adipiscing</li>
+				  		<li>Sed a viverra leo</li>
+	  				</ul>
+	  				<button class="button-group">Change color</button>
+				</div>
+			</div>
+		</div> -->
+	</div>
+
